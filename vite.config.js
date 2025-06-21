a762b44994c46161900bfc366188aef0ccef244e
pranab56 <pronabhalder53@gmail.com>
2025-06-21 16:58:39 +0600

    all ok

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
-// https://vite.dev/config/
+ 
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: '145.223.33.60', // Make sure the host is correctly set to 0.0.0.0 or localhost
-    host: '0.0.0.0',
-    port: 6002 // Or use another port if this one is already occupied
-  }
+    host: "145.223.33.60",
+    port: 4174,
+  },
+  build: {
+    sourcemap: false,  // disables source maps for production
+    minify: "esbuild", // faster minifier
+  },
 });
+ 
