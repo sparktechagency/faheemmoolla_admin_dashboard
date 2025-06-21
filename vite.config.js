2bbe0ec75e718fbef2bbb0883d37cb31bd478565
pranab56 <pronabhalder53@gmail.com>
2025-06-21 17:15:46 +0600

    all ok

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,35 @@
 import react from "@vitejs/plugin-react";
+
 import { defineConfig } from "vite";
- 
+
 export default defineConfig({
+
   plugins: [react()],
+
   server: {
-    host: "145.223.33.60",
+
+    host: "145.223.33.60",  // Your IP address (can be used to allow remote access)
+
     port: 4174,
+
+    allowedHosts: [
+
+      "admin.foodsavr.com",
+
+      "www.admin.foodsavr.com", // Add www version of your domain
+
+      "localhost"
+
+    ],
+
   },
+
   build: {
+
     sourcemap: false,  // disables source maps for production
+
     minify: "esbuild", // faster minifier
+
   },
+
 });
- 
